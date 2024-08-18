28fef4517dcf66b4421097c769d1edb7aa25391d
Andy <andypicke@gmail.com>
2024-08-18 09:09:50 -0600

    fix script to work for all links

--- scrape_library_locations.R
+++ scrape_library_locations.R
@@ -43,10 +43,20 @@ extract_info <- function(x){
   lib_name <- str_replace_all(lib_name, "\\+", " ") # note need to escape plus sign
   
   # extract lat,lng location (starts after "@")
-  lib_loc <- str_sub(x, ind2[1] + 2, ind2[1] + 23)
-  ind4 <- stringr::str_locate(lib_loc,",")
-  lib_lat <- as.numeric(str_sub(lib_loc, 1, ind4[1] - 1))
-  lib_lng <- as.numeric(str_sub(lib_loc, ind4[1] + 1))
+  lib_loc <- str_sub(x, ind2[1] + 2, nchar(x))
+  
+  # find first comma
+  ind_comma1 <- str_locate(lib_loc, ",")
+  
+  # get latitude
+  lib_lat <- as.numeric(str_sub(lib_loc, 1, ind_comma1[1] - 1))
+  
+  # find next comma
+  y <- str_sub(lib_loc, ind_comma1[1] + 1, nchar(lib_loc) - 2)
+  ind_comma2 <- str_locate(y, ",")
+  
+  # get longitude
+  lib_lng <- as.numeric(str_sub(y, 1, ind_comma2[1] - 1))
   
   df_out <- data.frame(lib_name, lib_lat, lib_lng)
 }
@@ -55,7 +65,7 @@ extract_info <- function(x){
 #df <- extract_info(gmap_links[9])
 
 # apply to all links to get dataframe of locations
-df <- purrr::map(gmap_links, extract_info) |> purrr::list_rbind()
+df <- purrr::map(gmap_links, extract_info) |> purrr::list_rbind() |> tidyr::drop_na()
 
 # plot locations on map
 leaflet() |>
